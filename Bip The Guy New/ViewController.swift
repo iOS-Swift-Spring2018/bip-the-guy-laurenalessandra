8083c6b72f4fd323cb076acc8698e55506a42c2a
Lauren Simon <laurenalessandra@laurens-macbook-pro-2.local>
2018-02-10 20:55:48 -0500

    Initial Commit

---
+++ Bip The Guy New/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bip The Guy New
+//
+//  Created by Lauren Simon on 2/10/18.
+//  Copyright © 2018 Simon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
